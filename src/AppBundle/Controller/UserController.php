42b6dabd634a2ef6ee3cd5aa6e4af44662653bef
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2016-11-12 14:42:04 +0100

    Define API for user

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
+
+
+/**
+ * User controller.
+ */
+class UserController extends FOSRestController
+{
+    /**
+     * Lists all user entities.
+     *
+     * @Rest\Get("user/", name="user_index")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->view($users, Response::HTTP_OK);
+    }
+
+    /**
+     * Creates a new user entity.
+     *
+     * @Rest\Post("user/new", name="user_new")
+     */
+    public function newAction(Request $request)
+    {
+
+        $data = json_decode($request->getContent(), true);
+
+        $prename = $data['Prename'];
+        $name = $data['Prename'];
+        $mail = $data['Prename'];
+        $user = new User();
+
+        $user->setName($name);
+        $user->setPrename($prename);
+        $user->setEmail($mail);
+        $user->setFriends(array());
+
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($mail) {
+            $existing_user = $em->getRepository('AppBundle:User')->findBy(array('email' => $mail));
+            if ($existing_user) {
+                $user = $existing_user;
+            }
+        }
+        else {
+            $em->persist($user);
+            $em->flush($user);
+        }
+
+        return $this->view($user,Response::HTTP_OK);
+    }
+
+    /**
+     * Finds and displays a user entity.
+     *
+     * @Rest\Get("user/{id}", name="user_show")
+     */
+    public function showAction(User $user)
+    {
+        return $this->view($user,Response::HTTP_OK);
+    }
+
+
+    /**
+     * Deletes a user entity.
+     *
+     * @Rest\Delete("user/{id}", name="user_delete")
+     */
+    public function deleteAction(User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush($user);
+
+        return $this->view($user,Response::HTTP_OK);
+    }
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $prename;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $email;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(type="array")
+     */
+    private $friends;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set prename
+     *
+     * @param string $prename
+     *
+     * @return User
+     */
+    public function setPrename($prename)
+    {
+        $this->prename = $prename;
+
+        return $this;
+    }
+
+    /**
+     * Get prename
+     *
+     * @return string
+     */
+    public function getPrename()
+    {
+        return $this->prename;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set friends
+     *
+     * @param string $friends
+     *
+     * @return array
+     */
+    public function setFriends($friends)
+    {
+        $this->friends = $friends;
+
+        return $this;
+    }
+
+    /**
+     * Get friends
+     *
+     * @return array
+     */
+    public function getFriends()
+    {
+        return $this->friends;
+    }
+}
