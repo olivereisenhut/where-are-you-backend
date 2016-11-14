7d0c86652155a77952661742333ba5760fc44a5e
Oliver Eisenhut <oliver.eisenhut@gmail.com>
2016-11-14 14:08:54 +0100

    Add coordinates to api

---
+++ src/AppBundle/Controller/CoordinateController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Coordinate;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
+
+
+/**
+ * Coordinate controller.
+ */
+class CoordinateController extends FOSRestController
+{
+    /**
+     * Get coordinate for user
+     *
+     * @Rest\Get("coordinate/{id}", name="coordinate_index")
+     */
+    public function showAction(Coordinate $coordinate)
+    {
+        return $this->view($coordinate, Response::HTTP_OK);
+    }
+
+    /**
+     * Update coordinate for user
+     *
+     * @Rest\Post("coordinate/{id}", name="coordinate_new")
+     */
+    public function updateAction(Request $request, User $user)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $long = $data['long'];
+        $lat = $data['lat'];
+
+        if ($long && $lat) {
+            $coordinate = new Coordinate();
+            $coordinate->setUserId($user->getId());
+            $coordinate->setLongitude($long);
+            $coordinate->setLatitude($lat);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($coordinate);
+            $em->flush($coordinate);
+        } else {
+            throw new HttpException(400, "No coordinates set");
+        }
+
+        return $this->view($coordinate, Response::HTTP_OK);
+    }
+}
---
+++ src/AppBundle/Entity/Coordinate.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Coordinate
+ *
+ * @ORM\Table(name="coordinate")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CoordinateRepository")
+ */
+class Coordinate
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="longitude", type="float")
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="latitude", type="float")
+     */
+    private $latitude;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Coordinate
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param \double $longitude
+     *
+     * @return Coordinate
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     *
+     * @return Coordinate
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+}
